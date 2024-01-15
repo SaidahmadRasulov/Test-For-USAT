f0e5a08422b81eaaebb469f81d5cc123c2d7e4df
SaidahmadRasulov <said,rasulov.2018@list.ru>
2024-01-15 15:05:49 +0500

    Changing and adding select value when send msg

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+function isValidPhoneNumber(phoneNumber) {
+  const phoneRegex = /\+\d{3}\d{2}\d{3}\d{2}\d{2}$/;
+  return phoneRegex.test(phoneNumber);
+}
 emailjs.init("8ERjlfNfMnEj4fFTP");
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
   SendMail();
-  ShowEnterBtn()
 });
 
-const select = document.getElementById('course');
+const select = document.getElementById("course");
 
-select.addEventListener('change', function() {
-  console.log(select.value)
-})
+select.addEventListener("change", function () {
+  console.log(select.value);
+});
 
 function SendMail() {
   const params = {
@@ -18,34 +21,37 @@ function SendMail() {
     school: document.getElementById("schoolL").value,
     class: document.getElementById("classRoom").value,
     parent: document.getElementById("parents").value,
-    course: document.getElementById('course').value
+    courses: document.getElementById("course").value,
   };
 
   if (
     params.fullName &&
-    params.phone &&
     params.school &&
     params.class &&
-    params.course &&
     params.parent !== ""
   ) {
-    emailjs
-      .send("service_ndpq0tw", "template_hskqdeosss", params)
-      .then(function (response) {
-        console.log("Email sent successfully:", response);
-      })
-      .catch(function (error) {
-        console.log("Error sending email:", error);
-      });
+    if (!isValidPhoneNumber(params.parent)) {
+      alert("Telefon raqam kiritilmagan. Iltimos telefon raqam kiriting.");
+    } else {
+      emailjs
+        .send("service_ndpq0tw", "template_hskqdeosss", params)
+        .then(function (response) {
+          console.log("Email sent successfully:", response);
+        })
+        .catch(function (error) {
+          console.log("Error sending email:", error);
+        });
+      ShowEnterBtn();
+    }
   } else {
-    alert('Hammasini toldiring!')
+    alert("Hammasini toldiring!");
   }
 }
 
-const box = document.querySelector('.box');
-const testingBox = document.querySelector('.entering_test');
+const box = document.querySelector(".box");
+const testingBox = document.querySelector(".entering_test");
 
 function ShowEnterBtn() {
-  box.classList.toggle('hidden');
-  testingBox.classList.toggle('visible')
+  box.classList.toggle("hidden");
+  testingBox.classList.toggle("visible");
 }
